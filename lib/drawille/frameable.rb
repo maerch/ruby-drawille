93b1a1db637f181a2e6fbdd72b990b703622ee2e
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-06-01 13:06:40 -0400

    Fix for position of the canvas

--- lib/drawille/frameable.rb
+++ lib/drawille/frameable.rb
@@ -14,19 +14,19 @@ module Drawille
     def row y, options={}
       chars = options[:chars] || @chars
       row   = chars[y]
-      min   = options[:min_x] || row.keys.min
-      max   = options[:max_x] || row.keys.max
+      min   = options[:min_x]
+      max   = options[:max_x]
       return "" if min.nil? || max.nil?
       (min..max).reduce("") { |memo, i| memo << to_braille(row.nil? ? 0 : row[i] || 0) }
     end
 
     def rows options={}
       chars = options[:chars] || @chars
-      min   = options[:min_y] || chars.keys.min
-      max   = options[:max_y] || chars.keys.max
+      min   = options[:min_y] || [chars.keys.min, 0].min
+      max   = options[:max_y] ||  chars.keys.max
       return [] if min.nil? || max.nil?
-      options[:min_x] ||= chars.reduce([]) { |m,x| m << x.last.keys }.flatten.min
-      options[:max_x] ||= chars.reduce([]) { |m,x| m << x.last.keys }.flatten.max
+      options[:min_x] ||= [chars.reduce([]) { |m,x| m << x.last.keys }.flatten.min, 0].min
+      options[:max_x] ||=  chars.reduce([]) { |m,x| m << x.last.keys }.flatten.max
       (min..max).map { |i| row i, options }
     end
 
